e53f09669d9d90f524ca19470927c6be8c4492ed
deborafelix <deboranunesfelix@gmail.com>
2021-08-18 21:28:18 -0300

    test: add create link controller test

--- __tests__/controller/CreateLinkController.test.ts
+++ __tests__/controller/CreateLinkController.test.ts
@@ -1,5 +1,26 @@
+import faker from 'faker';
+import CreateLinkController from '../../src/controllers/CreateLinkController';
+import CreateLinkService from '../../src/services/CreateLinkService';
+
 describe('Create Link Controller', () => {
-  it('should create a link', () => {
-    expect(true).toBe(true);
+  const spy = jest.spyOn(CreateLinkService.prototype, 'execute');
+
+  it('should create a link', async () => {
+    const payload = {
+      body: {
+        group: faker.random.word(),
+        url: faker.internet.url(),
+      },
+    };
+    spy.mockResolvedValue({
+      id: faker.datatype.uuid(),
+      group: payload.body.group,
+      url: payload.body.url,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    const sut = new CreateLinkController();
+    const result = await sut.handle(payload);
+    expect(result).toBeDefined();
   });
 });
--- src/interfaces/RequestPayloadInterface.ts
+++ src/interfaces/RequestPayloadInterface.ts
@@ -1,5 +1,5 @@
 export default interface IRequestPayload {
-  body: Record<string, any>,
-  params: Record<string, any>,
-  query: Record<string, any>
+  body?: Record<string, any>,
+  params?: Record<string, any>,
+  query?: Record<string, any>
 };
